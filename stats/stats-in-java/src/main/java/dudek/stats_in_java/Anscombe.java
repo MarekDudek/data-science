2880364b35eb2fa7510b3a6f0328c1e5f90e7926
Marek Dudek <marek.dudek@gmail.com>
2019-11-06 21:28:00 +0100

    entire Anscombe's quartet

--- stats/stats-in-java/src/main/java/dudek/stats_in_java/Anscombe.java
+++ stats/stats-in-java/src/main/java/dudek/stats_in_java/Anscombe.java
@@ -20,4 +20,49 @@ public enum Anscombe {
                     add(new Point2D(7.0, 4.82)).
                     add(new Point2D(5.0, 5.68)).
                     build();
+
+    public static final ImmutableList<Point2D> Sequence2 =
+            ImmutableList.<Point2D>builder().
+                    add(new Point2D(10.0, 9.14)).
+                    add(new Point2D(8.0, 8.14)).
+                    add(new Point2D(13.0, 8.74)).
+                    add(new Point2D(9.0, 8.77)).
+                    add(new Point2D(11.0, 9.26)).
+                    add(new Point2D(14.0, 8.10)).
+                    add(new Point2D(6.0, 6.13)).
+                    add(new Point2D(4.0, 3.10)).
+                    add(new Point2D(12.0, 9.13)).
+                    add(new Point2D(7.0, 7.26)).
+                    add(new Point2D(5.0, 4.74)).
+                    build();
+
+    public static final ImmutableList<Point2D> Sequence3 =
+            ImmutableList.<Point2D>builder().
+                    add(new Point2D(10.0, 7.46)).
+                    add(new Point2D(8.0, 6.77)).
+                    add(new Point2D(13.0, 12.74)).
+                    add(new Point2D(9.0, 7.11)).
+                    add(new Point2D(11.0, 7.81)).
+                    add(new Point2D(14.0, 8.84)).
+                    add(new Point2D(6.0, 6.08)).
+                    add(new Point2D(4.0, 5.39)).
+                    add(new Point2D(12.0, 8.15)).
+                    add(new Point2D(7.0, 6.42)).
+                    add(new Point2D(5.0, 5.73)).
+                    build();
+
+    public static final ImmutableList<Point2D> Sequence4 =
+            ImmutableList.<Point2D>builder().
+                    add(new Point2D(8.0, 6.58)).
+                    add(new Point2D(8.0, 5.76)).
+                    add(new Point2D(8.0, 7.71)).
+                    add(new Point2D(8.0, 8.84)).
+                    add(new Point2D(8.0, 8.47)).
+                    add(new Point2D(8.0, 7.04)).
+                    add(new Point2D(8.0, 5.25)).
+                    add(new Point2D(19.0, 12.50)).
+                    add(new Point2D(8.0, 5.56)).
+                    add(new Point2D(8.0, 7.91)).
+                    add(new Point2D(8.0, 6.89)).
+                    build();
 }
--- stats/stats-in-java/src/test/java/dudek/stats_in_java/AnscombeTest.java
+++ stats/stats-in-java/src/test/java/dudek/stats_in_java/AnscombeTest.java
@@ -1,19 +1,49 @@
 package dudek.stats_in_java;
 
-import org.junit.jupiter.api.Test;
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.ToString;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
-import static dudek.stats_in_java.Anscombe.Sequence1;
+import static dudek.stats_in_java.Anscombe.*;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 final class AnscombeTest {
 
-    @Test
-    void mean() {
+    @Builder
+    @ToString
+    private static class AnscombeTestCase {
+        @NonNull
+        public final List<Point2D> sequence;
+    }
+
+    private static Stream<AnscombeTestCase> anscombeTestCases() {
+        return Stream.of(
+                AnscombeTestCase.builder().
+                        sequence(Sequence1).
+                        build(),
+                AnscombeTestCase.builder().
+                        sequence(Sequence2).
+                        build(),
+                AnscombeTestCase.builder().
+                        sequence(Sequence3).
+                        build(),
+                AnscombeTestCase.builder().
+                        sequence(Sequence4).
+                        build()
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("anscombeTestCases")
+    void mean(final AnscombeTestCase tc) {
         // given
-        final List<Double> xs = Sequence1.stream().map(p -> p.x).collect(toList());
+        final List<Double> xs = tc.sequence.stream().map(p -> p.x).collect(toList());
         // when
         final double mean = Stats.mean(xs);
         // then
