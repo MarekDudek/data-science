5e4d084eebfe8397c01af045c3c842bf3382ba40
Marek Dudek <marek.dudek@gmail.com>
2020-02-22 17:15:50 +0100

    whole nyse data in HDF5

--- scipy-demo/nyse_daily/reading.py
+++ scipy-demo/nyse_daily/reading.py
@@ -1,3 +1,5 @@
+import glob
+from itertools import chain
 from zipfile import ZipFile
 import pandas as pd
 
@@ -7,5 +9,15 @@ def read_zip_with_csv(path):
     dfs = [pd.read_csv(zip_file.open(zip_info.filename)) for zip_info in zip_file.infolist()]
     return pd.concat(dfs)
 
-def read_zips_with_csv(paths):
-    pass
+
+def to_infos_with_file(zip_file):
+    return map(lambda zip_info: (zip_file, zip_info), zip_file.infolist())
+
+
+def read_zips_with_csv(pattern):
+    paths = [path for path in glob.glob(pattern)]
+    zip_files = [ZipFile(path) for path in paths]
+    infos_with_file = [to_infos_with_file(zip_file) for zip_file in zip_files]
+    infos_with_file = chain.from_iterable(infos_with_file)
+    dfs = [pd.read_csv(zip_file.open(zip_info.filename)) for (zip_file, zip_info) in infos_with_file]
+    return pd.concat(dfs)
